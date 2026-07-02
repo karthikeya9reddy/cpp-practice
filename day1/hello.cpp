dd1b4a178e99be7991eee127fed14fae646090b2
karthikeya9reddy <ammulukarthik9@gmail.com>
2026-07-02 22:29:24 +0530

    Update hello.cpp

--- day1/hello.cpp
+++ day1/hello.cpp
@@ -9,10 +9,20 @@ int main() {
 
 
 
-#explanation- 
+// explanation 
 
-<iostram> -: header file that provides functionalities for input and output operations in C++. It allows the program to read from standard input (like keyboard) and write to standard output (like console).   
-int main() -: This is the main function where the execution of the program begins. It returns an integer value to the operating system upon completion.
-std::cout -: This is an object of the output stream class that is used to output data to the console.
-cout << "hi my name is karthikeya"; -: This line uses the output stream object to print the string "hi my name is karthikeya" to the console.
-return 0; -: This statement indicates that the program has executed successfully and returns 0 to the operating system.
+// <iostram> :- is a header file in C++ that provides functionalities for input and output operations. It allows the program to read from standard input (like keyboard) and write to standard output (like console).
+
+// int main() :- This is the main function where the execution of the program begins. It is the entry point of a C++ program.
+
+// std:: :- This is the namespace that contains all the standard C++ library functions and objects. Using std:: allows us to access these functions without having to define them ourselves.       
+
+// cout :- This is an object of the ostream class that is used to output data to the standard output stream (console).
+
+// << :- This is the insertion operator that is used to send data to the output stream (in this case, to cout).
+
+// "hi my name is karthikeya" :- This is a string literal that will be printed to the console when the program is executed.
+
+// return 0; :- This statement indicates that the program has executed successfully. It returns the value 0 to the operating system, which is a common convention to indicate successful completion of a program.
+
+//  '//' is used to write single-line comments in C++. Comments are ignored by the compiler and are used to provide explanations or notes within the code for better understanding.
