8373a0a43770115b377f35d80a6b594aabc425f3
karthikeya9reddy <ammulukarthik9@gmail.com>
2026-07-03 00:44:56 +0530

    learned variables and comments

---
+++ day1/concp2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 123;
+    int b = 456;
+    float pie = 3.14;
+    char c = 'p';
+    cout << "the value of a is : " << a << "\n the value of b is : " << b << "\n the sum of a and b is : " << a + b << "\n the value of pie is : " << pie << "\n the value of c is : " <<c<< "\nthe sum of a and b and pie is: "<< a +b +pie;
+    return 0;
+}
+
+
+// explanation
+
+// int = - This is a data type in C++ that is used to declare variables that can hold integer values (whole numbers). In this code, 'a' and 'b' are declared as integers.
+// float = - This is a data type in C++ that is used to declare variables that can hold floating-point numbers (numbers with decimal points). In this code, 'pie' is declared as a float.
+// char = - This is a data type in C++ that is used to declare variables that can hold single characters. In this code, 'c' is declared as a char.  
+//double = - This is a data type in C++ that is used to declare variables that can hold double-precision floating-point numbers (numbers with decimal points). In this code, 'pie' could also be declared as a double for more precision, but it is declared as a float here.   
+// boolean = - This is a data type in C++ that is used to declare variables that can hold boolean values (true or false). In this code, there are no boolean variables declared, but they can be used in conditional statements and logical operations. 
