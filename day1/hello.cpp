1bf59975d41b7b0d84b85929c1c33d2a4ac1bce4
karthikeya9reddy <ammulukarthik9@gmail.com>
2026-07-02 14:38:39 +0530

    Update hello.cpp

--- day1/hello.cpp
+++ day1/hello.cpp
@@ -1,7 +1,18 @@
 #include <iostream> 
 
 int main() {
+
     std::cout << "hi my name is karthikeya";
     return 0;
 }
 
+
+
+
+#explanation- 
+
+<iostram> -: header file that provides functionalities for input and output operations in C++. It allows the program to read from standard input (like keyboard) and write to standard output (like console).   
+int main() -: This is the main function where the execution of the program begins. It returns an integer value to the operating system upon completion.
+std::cout -: This is an object of the output stream class that is used to output data to the console.
+cout << "hi my name is karthikeya"; -: This line uses the output stream object to print the string "hi my name is karthikeya" to the console.
+return 0; -: This statement indicates that the program has executed successfully and returns 0 to the operating system.
