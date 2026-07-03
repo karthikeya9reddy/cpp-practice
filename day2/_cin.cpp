f2d64b9e84cdb896e5539cf2d8d7cfcc17c8cbe0
karthikeya9reddy <ammulukarthik9@gmail.com>
2026-07-03 23:30:25 +0530

    input function covered

---
+++ day2/_cin.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int sum1, sum2;
+
+//     cout << "enter the value of sum1 :";
+//     cin >> sum1; 
+    
+//     cout << "\nenter the value of sum2 :";
+//     cin >> sum2; 
+// cout << "\n the sum of1 sum1 and sum2 is :"<<sum1+sum2;
+// return 0;
+      
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum1, sum2;
+
+    cout << "enter the value of sum1 :";
+    cin >> sum1; 
+    
+    cout << "\nenter the value of sum2 :";
+    cin >> sum2; 
+cout << "\n the multiplication of sum1 and sum2 is :"<<sum1*sum2;
+return 0;
+      
+}
