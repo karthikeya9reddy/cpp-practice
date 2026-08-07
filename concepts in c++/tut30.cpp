10072c5b1e0309450772ce0e45453f70ab59db47
karthikeya9reddy <ammulukarthik9@gmail.com>
2026-08-07 21:05:29 +0530

    concepts in c++

---
+++ concepts in c++/tut30.cpp
@@ -0,0 +1,26 @@
+// constructor with default arguments 
+#include <iostream>
+using namespace std;
+
+class simple{
+int data1, data2, data3;
+
+public:
+     simple(int a, int b=9, int c=11){
+        data1 = a;
+        data2 = b;
+        data3 = c;
+     }
+     void printdata();
+};
+
+void simple :: printdata(){
+     cout<<"the vlaue of data1,data2 and data3 is "<<data1<<","<<data2 <<" and "<<data3<<endl;
+}
+
+int main(){
+    simple s(1);
+    s.printdata();
+    
+    return 0;
+}
