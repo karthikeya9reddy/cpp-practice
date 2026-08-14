16190de53f8cdb3dd8358f1e7f2577bcf7498afc
karthikeya9reddy <ammulukarthik9@gmail.com>
2026-08-14 12:22:51 +0530

    concepts in c++

---
+++ concepts in c++/tut35b.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class base
+{
+    int data1; // private by default and is not inheritable
+public:
+    int data2;
+    void setdata();
+    int getdata1();
+    int getdata2();
+};
+void base ::setdata(void)
+{
+    data1 = 10;
+    data2 = 20;
+}
+
+int base ::getdata1()
+{
+    return data1;
+}
+
+int base ::getdata2()
+{
+    return data2;
+}
+class derived : private base
+{ // public data of derived class is called privately
+    int data3;
+
+public:
+    void process();
+    void display();
+};
+
+void derived ::process()
+{
+    setdata(); // can call base class private data only by public data of derived class
+    data3 = data2 * getdata1();
+}
+
+void derived ::display()
+{
+    cout << "value of data 1 is " << getdata1() << endl;
+    cout << "value of data 2 is " << data2 << endl;
+    cout << "value of data 3 is " << data3 << endl;
+}
+int main()
+{
+    derived der;
+    // der.setdata();
+    der.process();
+    der.display();
+    return 0;
+}
