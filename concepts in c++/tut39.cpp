f49b184dc8258aa296ab5b6fe2c1ddc31a7c07d4
karthikeya9reddy <ammulukarthik9@gmail.com>
2026-08-21 22:21:50 +0530

    concepts in c++

---
+++ concepts in c++/tut39.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class base1{
+    public:
+    void greet(){
+        cout<<"how are you?"<<endl;
+    }
+};
+
+class base2{
+    public:
+    void greet(){
+        cout<<"yettal unnav antha manchee naa?"<<endl;
+    }
+};
+
+class derived: public base1, public base2{
+    int a; 
+    public:
+    void greet(){
+     base2::greet();
+    }
+};
+
+class B{
+    public:
+    void say(){
+        cout<<"hello world!"<<endl;
+    }
+};
+
+class D: public B{
+    int b;
+    public:
+    void say(){
+        cout<<"hello beautiful ppl!"<<endl;
+    }
+};
+
+int main(){
+    //Ambiguity 1
+    // base1 base1obj;
+    // base2 base2obj;
+    // base1obj.greet();
+    // base2obj.greet();
+    // derived d;
+    // d.greet();
+    // return 0;
+
+    //Ambiguity 2
+    B b;
+    b.say();
+    D d;
+    d.say();
+}
