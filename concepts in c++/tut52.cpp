5c10d248ff9e4358a8bbcbed9de632175647bf8e
karthikeya9reddy <ammulukarthik9@gmail.com>
2026-09-06 14:31:22 +0530

    concepts in c++

---
+++ concepts in c++/tut52.cpp
@@ -0,0 +1,17 @@
+// File I/O in C++: Working with Files
+
+/*
+What is a file? (1:01 - 2:46): At a foundational level, a file is a data pattern stored on a disk. It consists of characters that, when viewed as bits (binary 0s and 1s), represent information.
+
+Binary and Computers (3:02 - 10:52): The video explains how data is represented using the ASCII standard and converted into binary. Since computers operate using switches (transistors) that are either on or off, binary is the fundamental language they understand.
+
+Interaction with Programs (11:54 - 15:52): The speaker illustrates how a C++ program interacts with the outside world. While cin and cout are used for keyboard and console input/output, file handling involves a similar concept where the program reads from or writes to a file stored on the disk.
+
+Streams (16:10 - 17:35): A stream acts as an interface for data transfer.
+
+Input stream: Used to read data from a file into the program.
+
+Output stream: Used to write data from the program into a file.
+
+By the end of the video, the groundwork is laid for upcoming tutorials where the actual implementation of reading and writing files will be demonstrated.
+*/
